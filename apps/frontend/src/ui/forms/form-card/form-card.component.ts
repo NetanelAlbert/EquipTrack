c721eb0ad95a0d89d86ae666f76ff0241f6c4e59
Nati Albert <netanela@carbyne.com>
2025-06-14 00:11:53 +0300

    Add approval and rejection buttons to form-card

--- apps/frontend/src/ui/forms/form-card/form-card.component.ts
+++ apps/frontend/src/ui/forms/form-card/form-card.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { InventoryListComponent } from '../../inventory/list/inventory-list.component';
 import { InventoryForm } from '@equip-track/shared';
+import { SignaturePadComponent } from '../../signature-pad/signature-pad.component';
 
 @Component({
   selector: 'app-form-card',
@@ -13,10 +14,22 @@ import { InventoryForm } from '@equip-track/shared';
     MatIconModule,
     TranslateModule,
     InventoryListComponent,
+    SignaturePadComponent,
   ],
   templateUrl: './form-card.component.html',
   styleUrls: ['./form-card.component.scss'],
 })
 export class FormCardComponent {
   @Input() form!: InventoryForm;
+
+  signatureData = '';
+
+  onApprove() {
+    console.log('form Approve', this.form.formID);
+    
+  }
+
+  onReject() {
+    console.log('form Reject', this.form.formID);
+  }
 }
