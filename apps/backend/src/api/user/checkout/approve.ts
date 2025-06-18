ac94e03307080aa0bbbff7d59f8eb5e28ebbb80d
Nati Albert <netanela@carbyne.com>
2025-06-19 00:23:29 +0300

    implement approve checkout handler

--- apps/backend/src/api/user/checkout/approve.ts
+++ apps/backend/src/api/user/checkout/approve.ts
@@ -1,4 +1,10 @@
 import { User, ApproveCheckOut, BasicResponse } from '@equip-track/shared';
 // import { InventoryFormAdapter } from '../../../../db/tables/inventory-form.adapter';
 
-
+export const handler = async (
+  user: User,
+  req: ApproveCheckOut
+): Promise<BasicResponse> => {
+  // TODO: Use InventoryFormAdapter to approve checkout
+  return { status: true };
+};
