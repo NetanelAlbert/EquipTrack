e26ab1dda938b98e7ef3435672ccae9d72207358
Nati Albert <netanela@carbyne.com>
2025-07-21 01:55:38 +0300

    Remove process  API URL from environment configuration

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiUrl:
-    process.env['API_URL'] ||
     'https://u2nfggttwk.execute-api.il-central-1.amazonaws.com',
   googleClientId:
     '64930861221-3571tfrilm698f11h0p15ph8hi4klt1j.apps.googleusercontent.com',
--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl:
-    process.env['API_URL'] ||
     'https://u2nfggttwk.execute-api.il-central-1.amazonaws.com/dev',
   googleClientId:
     '64930861221-3571tfrilm698f11h0p15ph8hi4klt1j.apps.googleusercontent.com',
