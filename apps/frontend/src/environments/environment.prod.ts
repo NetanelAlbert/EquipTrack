425bfe60f348d6ecdddfbe81e65abfb10d0444ff
Nati Albert <netanela@carbyne.com>
2025-07-21 01:36:49 +0300

    Refactor environment configuration to use environment variables for API URL

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://u2nfggttwk.execute-api.il-central-1.amazonaws.com',
-  googleClientId: '64930861221-3571tfrilm698f11h0p15ph8hi4klt1j.apps.googleusercontent.com'
+  apiUrl:
+    process.env['API_URL'] ||
+    'https://u2nfggttwk.execute-api.il-central-1.amazonaws.com',
+  googleClientId:
+    '64930861221-3571tfrilm698f11h0p15ph8hi4klt1j.apps.googleusercontent.com',
 };
