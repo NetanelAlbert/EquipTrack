198e7051b120325d3d659f488516b71569d1b783
GitHub Action <action@github.com>
2025-08-06 17:35:59 +0000

    chore: bump version to 0.0.11 [skip version]

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.equip-track.com',
   googleClientId:
     '64930861221-3571tfrilm698f11h0p15ph8hi4klt1j.apps.googleusercontent.com',
-  version: '0.0.10', // This will be updated by the build process
+  version: '0.0.11', // This will be updated by the build process
 };
